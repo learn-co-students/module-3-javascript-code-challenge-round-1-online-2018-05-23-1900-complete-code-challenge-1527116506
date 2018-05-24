a5371071096eda37c58e40422fd888fbd21e417b
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-05-23 20:11:45 -0400

    code challenge

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const imageId = 1 //Enter your assigned imageId here
+  const imageId = 13 //Enter your assigned imageId here
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
   const likeURL = `https://randopic.herokuapp.com/likes/`
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
 })
+
+function getImage() {
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showImage);
+  req.open("GET", 'https://randopic.herokuapp.com/images/13')
+  req.send()
+}
+
+function showImage(event, data) {
+  var response = JSON.parse(this.responseText)
+  console.log(response)
+  const content = $('.div_imagetranscrits').html('<img src="data:image/png;base64,' + response.url + '" />');
+  document.getElementById("image").src = content
+}
